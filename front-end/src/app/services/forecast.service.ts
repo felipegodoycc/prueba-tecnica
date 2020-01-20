b0efce597de62110af3bcb826825686e0b7a65a8
Felipe Godoy <felipe.godoy@ceinf.cl>
2020-01-19 22:40:56 -0300

    fix menor

--- front-end/src/app/services/forecast.service.ts
+++ front-end/src/app/services/forecast.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, retryWhen, delay, tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable({
