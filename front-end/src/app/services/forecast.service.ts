4ef6f912eaf9873eeed6aa65c1eff2297bd8d115
Felipe Godoy <felipe.godoy@ceinf.cl>
2020-01-19 22:40:24 -0300

    fix ruta

--- front-end/src/app/services/forecast.service.ts
+++ front-end/src/app/services/forecast.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, retryWhen, delay, tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForecastService {
-  apiUrl = enviroment.apiURL;
+  apiUrl = environment.apiURL;
 
   constructor(private http: HttpClient) { }
 
